bb5e299a933fdaaea3ccaa6c075cef15d912c019
wonangs <151820502+wonangs@users.noreply.github.com>
2023-12-29 14:41:55 +0900

    [Bronze V] Title: 합, Time: 200 ms, Memory: 17632 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = 0;
+
+        for (int i = 0; i <= a; i++) {
+            b += i;
+        }
+
+        System.out.println(b);
+    }
+}
