f3756acc657dc27054cf93870aef50b2f976aec7
wonangs <151820502+wonangs@users.noreply.github.com>
2024-01-10 21:05:52 +0900

    [Bronze V] Title: A+B - 4, Time: 220 ms, Memory: 18216 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -15,7 +15,7 @@ class Main {
 
         while ((str = br.readLine()) != null) { // 빈값이 아닐 때까지 실행한다
 
-
+            // 해당하는 정수의 숫자를 입력하기위해 아스키코드 -48을 뺀 값을 입력한다
             int a = str.charAt(0) - 48;
             int b = str.charAt(2) - 48;
             sb.append(a + b + "\n");
