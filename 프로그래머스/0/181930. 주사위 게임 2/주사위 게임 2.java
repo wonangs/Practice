c278531e6b89da209f06fdfa0a48f4f06c11346a
워낭 <151820502+wonangs@users.noreply.github.com>
2024-04-12 15:34:44 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -4,10 +4,11 @@ class Solution {
         
         if(a != b && a != c && b != c)
             answer = a + b + c;
-        else if((a == b && a != c) || (a == c && a != b) || (b == c && b != a))
-            answer = (a + b + c)*(a*a + b*b + c*c);
-        else
+        else if(a == b && b == c)
             answer = (a + b + c)*(a*a + b*b + c*c)*(a*a*a + b*b*b + c*c*c);
+        else
+            answer = (a + b + c)*(a*a + b*b + c*c);
+        
         
         return answer;
     }
