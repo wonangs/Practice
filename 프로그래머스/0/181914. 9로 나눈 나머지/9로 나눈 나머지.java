e77a45250f16e30b646a6e1f1b2f756f0e36e67f
워낭 <151820502+wonangs@users.noreply.github.com>
2024-04-26 02:50:16 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 0.03 ms, Memory: 74.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -0,0 +1,13 @@
+class Solution {
+        public int solution(String number) {
+            int answer = 0;
+            int temp = 0;
+
+            for (int i = 0; i < number.length(); i++) {
+                
+                temp += Integer.parseInt(String.valueOf(number.charAt(i)));
+            }
+            answer = temp % 9;
+            return answer;
+        }
+    }
