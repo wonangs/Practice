3c66faaccc9ffed99f2c3417acd71cc42841497d
워낭 <151820502+wonangs@users.noreply.github.com>
2024-04-29 17:15:50 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 132 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -6,36 +6,40 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        // N과 M을 입력받음
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        int[] basket = new int[N + 1]; // 바구니 초기화
-
-        // 처음에는 바구니 번호 순서대로 초기화
-        for (int i = 1; i <= N; i++) {
-            basket[i] = i;
+        // 바구니 배열 생성 및 초기화
+        int[] baskets = new int[N];
+        for (int i = 0; i < N; i++) {
+            baskets[i] = i + 1;
         }
 
+        // M번의 바구니 순서 역순 변환
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-
-            // start부터 end까지의 범위를 역순으로 바꿈
-            reverse(basket, start, end);
+            int start = Integer.parseInt(st.nextToken()) - 1; // 0부터 시작하도록 인덱스 조정
+            int end = Integer.parseInt(st.nextToken()) - 1; // 0부터 시작하도록 인덱스 조정
+            reverseBaskets(baskets, start, end);
         }
 
-        for (int i = 1; i <= N; i++) {
-            System.out.print(basket[i] + " ");
+        // 바구니에 적힌 순서 출력
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            sb.append(baskets[i]).append(" ");
         }
+        System.out.println(sb);
     }
 
-    private static void reverse(int[] basket, int start, int end) {
+    // 배열의 일부를 역순으로 만드는 메소드
+    private static void reverseBaskets(int[] baskets, int start, int end) {
         while (start < end) {
-            int temp = basket[start];
-            basket[start] = basket[end];
-            basket[end] = temp;
+            int temp = baskets[start];
+            baskets[start] = baskets[end];
+            baskets[end] = temp;
             start++;
             end--;
         }
