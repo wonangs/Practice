3f29ee0a227729927b7677e7d279692e5872f317
워낭 <151820502+wonangs@users.noreply.github.com>
2024-04-29 17:14:13 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 144 ms, Memory: 15864 KB -BaekjoonHub

--- 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -1,10 +1,14 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int N = scanner.nextInt();
-        int M = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
         int[] basket = new int[N + 1]; // 바구니 초기화
 
@@ -14,8 +18,9 @@ public class Main {
         }
 
         for (int i = 0; i < M; i++) {
-            int start = scanner.nextInt();
-            int end = scanner.nextInt();
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
             // start부터 end까지의 범위를 역순으로 바꿈
             reverse(basket, start, end);
@@ -24,7 +29,6 @@ public class Main {
         for (int i = 1; i <= N; i++) {
             System.out.print(basket[i] + " ");
         }
-        scanner.close();
     }
 
     private static void reverse(int[] basket, int start, int end) {
