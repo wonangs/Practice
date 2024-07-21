289c1723908c7c7050b3942704e32cb7872f96db
워낭 <151820502+wonangs@users.noreply.github.com>
2024-07-21 16:01:24 +0900

    [level 0] Title: 문자열 묶기, Time: 6.25 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] strArr) {
+        // 1부터 30까지 길이별 수 저장할 배열
+        int[] cnt = new int[31];
+
+        // 각 문자열의 길이에 따라 배열의 값을 증가
+        for (String str : strArr) {
+            int length = str.length();
+            cnt[length]++;
+        }
+
+        // 최대값 찾기
+        int max = 0;
+        for (int count : cnt) {
+            if (count > max) {
+                max = count;
+            }
+        }
+
+        return max;
+    }
+}
