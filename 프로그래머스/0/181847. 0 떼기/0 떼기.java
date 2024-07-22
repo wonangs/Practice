5dcff316c42d23056193ccd16c638e0390998624
워낭 <151820502+wonangs@users.noreply.github.com>
2024-07-22 12:02:47 +0900

    [level 0] Title: 0 떼기, Time: 8.20 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,7 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        int num = Integer.parseInt(n_str);
-        
-        return Integer.toString(num);
+        return Integer.parseInt(n_str) + "";
     }
 }
